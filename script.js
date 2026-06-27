2a7f0f5762596e3bade227288ff18fc5b208e9a8
g-techcelulares <gtechcelulares3@gmail.com>
2026-06-27 02:12:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// G. TECH CELULARES
+// Script inicial
 
+console.log("Site G. TECH CELULARES carregado com sucesso!");
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const destino = document.querySelector(this.getAttribute('href'));
+
+        destino.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
